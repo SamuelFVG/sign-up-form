edfd1cc6bb79cff2a774793cf937b025b93305f8
SamuelFVG <samuelfelipeif@gmail.com>
2022-01-31 17:00:19 -0300

    Added validation for both the password and the confirmation field

--- scripts/script.js
+++ scripts/script.js
@@ -56,6 +56,57 @@ function validateEmail() {
     else return true;
 }
 
+function validatePassword() {
+    if (!checkEmptiness(2)) {
+        const hasUpperCase = inputs[2].value.match(/[A-Z]/g);
+        const hasLowerCase = inputs[2].value.match(/[a-z]/g);
+        const hasNumber = inputs[2].value.match(/[0-9]/g);
+        const isGreaterThan6 = inputs[2].value.length >= 6;
+        /* prompt();
+        alert(`${hasUpperCase}\n${hasLowerCase}\n${hasNumber}\n${isGreaterThan6}`); */
+
+        if (!isGreaterThan6) {
+            statusTexts[2].textContent = 'Should have at least six characters';
+            statusIcons[2].classList.add('invalid');
+        }
+        else if (!hasLowerCase) {
+            statusTexts[2].textContent = 'Should have at least one lower case letter';
+            statusIcons[2].classList.add('invalid');
+        }
+        else if (!hasNumber) {
+            statusTexts[2].textContent = 'Should have at least one number';
+            statusIcons[2].classList.add('invalid');
+        }
+        else if (!hasUpperCase) {
+            statusTexts[2].textContent = 'Should have at least one upper case letter';
+            statusIcons[2].classList.add('invalid');
+        } 
+        else {
+            statusIcons[2].classList.add('valid');
+        }
+    }
+
+    if (statusTexts[2].textContent != '') return false;
+    else return true;
+}
+
+function validateConfirmedPassword() {
+    if(!checkEmptiness(5)) {   
+        if (statusTexts[2].textContent != '') {
+            statusTexts[5].textContent = "Invalid password";
+            statusIcons[5].classList.add('invalid');
+        }
+        if (inputs[5].value != inputs[2].value) {
+            statusTexts[5].textContent = "Passwords don't match";
+            statusIcons[5].classList.add('invalid');
+        } else {
+            statusIcons[5].classList.add('valid');
+        }
+    }
+    if (statusTexts[5].textContent != '') return false;
+    else return true;
+}
+
 function validate(event) {
     for (let i = 0; i < statusTexts.length; i++) {
         statusTexts[i].textContent = '';
@@ -63,8 +114,11 @@ function validate(event) {
     }
 
     const nameValidation = validateName(),
-        emailValidation = validateEmail();
-    if (nameValidation && emailValidation) shouldSubmit = true;
+        emailValidation = validateEmail(), 
+        passwordValidation = validatePassword(),
+        passwordConfirmValidation = validateConfirmedPassword();
+
+    if (nameValidation && emailValidation && passwordValidation && passwordConfirmValidation) shouldSubmit = true;
     if(!shouldSubmit) event.preventDefault();
 }
 
