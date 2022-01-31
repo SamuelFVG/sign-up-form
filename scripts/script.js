57e9e9896c8c1a7e475e5161103bb597a1acbfff
SamuelFVG <samuelfelipeif@gmail.com>
2022-01-31 16:10:26 -0300

    Added email validation

--- scripts/script.js
+++ scripts/script.js
@@ -5,23 +5,54 @@ const submitBtn = document.querySelector('button');
 
 let shouldSubmit = false;
 
+function checkEmptiness(i) {
+    if (inputs[i].value === '') {
+        statusTexts[i].textContent = 'Should not be empty!';
+        statusIcons[i].classList.add('invalid');    
+        return true;
+    } 
+
+    return false;
+}
+
+
 function validateName() {
-    for (let i = 0; i <= 3; i += 3){ //i will be one and three
+    for (let i = 0; i <= 3; i += 3){
         inputs[i].value = inputs[i].value.trim();
-        if (inputs[i].value === '') {
-            statusTexts[i].textContent = 'Name should not be empty!';
-            statusIcons[i].classList.add('invalid');    
+        
+        if (checkEmptiness(i)){
+            continue;
         }
         else if (inputs[i].value.includes(' ')) {
             statusTexts[i].textContent = 'Type only one name';
             statusIcons[i].classList.add('invalid');
         }
         else {
-            statusTexts[i].textContent = '';
             statusIcons[i].classList.add('valid');            
         }
     }
-    if (statusTexts[0].textContent != '' || statusTexts[1].textContent != '') return false;
+
+    if (statusTexts[0].textContent != '' || statusTexts[3].textContent != '') return false;
+    else return true;
+}
+function validateEmail() {
+    const matchingString = inputs[1].value.match(/(\w(\.|\-)?)*\w@\w(\w*)+\.+\w(\w*)+(\.?\w)*/gi);
+    
+    if (!checkEmptiness(1)) {
+        if (!matchingString) {
+            statusTexts[1].textContent = 'Invalid email address';
+            statusIcons[1].classList.add('invalid');
+        }
+        else{
+            if (matchingString[0] == inputs[1].value) {
+                statusIcons[1].classList.add('valid');            
+            } else {
+                statusTexts[1].textContent = 'Invalid email address';
+                statusIcons[1].classList.add('invalid');
+            }
+        }
+    }
+    if (statusTexts[1].textContent != '') return false;
     else return true;
 }
 
@@ -31,7 +62,9 @@ function validate(event) {
         statusIcons[i].classList = 'status-icon';
     }
 
-    shouldSubmit = validateName();    
+    const nameValidation = validateName(),
+        emailValidation = validateEmail();
+    if (nameValidation && emailValidation) shouldSubmit = true;
     if(!shouldSubmit) event.preventDefault();
 }
 
